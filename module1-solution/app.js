74e341f0823e62c3d3654f4d0d2c7369de706f6e
Imran Kazi <sujoy.imran@gmail.com>
2016-12-26 17:38:51 +0530

    further revision on module 1

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -8,6 +8,9 @@
   function LunchCheckController($scope){
       $scope.items = "";
       $scope.message =  "";
+      $scope.state = {};
+      $scope.state.color = "green";
+      $scope.state.bordercolor = "border-green";
 
       $scope.checkIfTooMuch =  function(){
         var m = "";
@@ -15,7 +18,11 @@
 
         var itemToken = items.split(',');
         var emptyTokensCount = numOfEmptyTokens(itemToken);
-        if(itemToken == "") m = "Please enter data first";
+        if(itemToken == "") {
+          m = "Please enter data first";
+          $scope.state.color = "red";
+          $scope.state.bordercolor = "border-red";
+        }
         else if(itemToken.length - emptyTokensCount > 3)  m = "Too Much!";
         else m = "Enjoy";
         $scope.message = m;
